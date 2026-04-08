2db86fcfa8f2fa1ec00ccf2588aff983690c69c9
sanjay_kumar <sanjay17126@gmail.com>
2026-04-08 18:44:03 +0000

    cleaned, and getting ready for nextjs app

--- client/src/components/common/index.ts
+++
@@ -1,8 +0,0 @@
-export { ToastProvider } from './ToastProvider';
-export { Pagination } from './Pagination';
-export { FileUpload } from './FileUpload';
-export { ErrorBoundary } from './ErrorBoundary';
-export { NotificationCenter } from './NotificationCenter';
-export { NepaliDatePicker } from './NepaliDatePicker';
-export { LanguageSwitcher, LanguageToggle } from './LanguageSwitcher';
-export { GlobalSearch } from './GlobalSearch';
--- server/prisma/migrations/20260127191349_add_company_member_isactive/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "CompanyMember" ADD COLUMN     "isActive" BOOLEAN NOT NULL DEFAULT true;
-
--- CreateIndex
-CREATE INDEX "CompanyMember_isActive_idx" ON "CompanyMember"("isActive");
